7d3b8fa593a71146db1500313e746ffe2c4f3b70
AhtiAnk <ahtiankro@gmail.com>
2023-03-10 00:25:36 +0200

    walking and bus between buildings

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,92 @@
 import './App.css';
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import Axios from "axios";
 
 function App() {
   const [buildings, setBuildings] = useState([]);
 
-
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  
+  
   const getBuildings = () => {
     Axios.get("http://localhost:3001/buildings").then((response) => {
       setBuildings(response.data);
     });
   };
-
+  
   useEffect(() => {
     getBuildings();
   },[])
+  
+  const onSubmit = data => {
+    window.open(`https://www.google.com/maps/dir/?api=1&origin=${data.startLocation}&destination=${data.endLocation}&&travelmode=${data.method}`, '_blank');
+  };
+    
 
   return (
-    <div className="App">
-      {buildings.map((val, key) => {
-        return <div>
-          {val.name} | {val.address}
+    <>
+      <nav className="navbar navbar-light bg-primary">
+        <div className="container-fluid">
+          <p className="navbar-brand text-white">Kodu</p>
+          <button class="btn btn-dark" type="button" href="/login.html">Logi sisse</button>
+          {/* <a className="nav-link text-white" href="/logi.html" tabindex="-1" >Logi sisse</a> */}
         </div>
-      })}
-    </div>
+      </nav>
+      <div className="container py-5">
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="row justify-content-md-center"> 
+            <div className="col-md-4">
+              <select className="form-select"
+              {...register("startLocation", { required: "Palun vali alguspunkt" })}
+              >
+                <option value="">Vali alguspunkt</option>
+                {buildings.map((val, key) => {
+                  return <option value={val.address}>{val.name} | {val.address}</option>
+                })}
+              </select>
+              {errors.startLocation && <span>{errors.startLocation.message}</span>}
+            </div>
+          </div>
+
+          <br/>
+
+          <div className="row justify-content-md-center"> 
+            <div className="col-md-4">
+              <select className="form-select"
+              {...register("endLocation", { required: "Palun vali lõpppunkt" })}
+              >
+                <option value="">Vali lõppunkt</option>
+                {buildings.map((val, key) => {
+                  return <option value={val.address}>{val.name} | {val.address}</option>
+                })}
+              </select>
+              {errors.endLocation && <span>{errors.endLocation.message}</span>}
+            </div>
+          </div>
+
+          <br/>
+          
+          <div className="row justify-content-md-center"> 
+            <div className="col-md-4">
+              <select className="form-select"
+              {...register("method", { required: "Palun vali liikumismeetod" })}
+              >
+                <option value="">Vali liikumise meetod</option>
+                <option value="walking">Kõndimine</option>
+                <option value="transit">Buss</option>
+              </select>
+              {errors.method && <span>{errors.method.message}</span>}
+            </div>
+          </div>
+          
+          <div className="d-grid col-md-2 mx-auto">
+            <input type="submit" className="btn btn-primary my-3" value="Näita marsruuti"/>
+          </div>
+        </form>        
+      </div>
+    </>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -23,6 +24,48 @@ app.get("/buildings", (req, res) => {
         }
     });
 });
+
+const link = "https://gis.tartulv.ee/arcgis/rest/services/Linnatransport/LI_rattaringluse_parklad_avaandmed/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson";
+
+const getBicycleStops = () => {
+    axios.get(link)
+    .then(result => {
+
+        result.data.features.forEach(element => {
+            //console.log(element.properties.Asukoht)
+            //console.log(typeof element.geometry.coordinates[0])
+            db.query(`INSERT INTO bicycleStops (address, x_coordinate, y_coordinate) VALUES ('${element.properties.Asukoht}', ${element.geometry.coordinates[0]}, ${element.geometry.coordinates[1]})`, (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    })
+  };
+
+//getBicycleStops();  
+
+/* app.get("/bicyclesStops", (req, res) => {
+    const link = "https://gis.tartulv.ee/arcgis/rest/services/Linnatransport/LI_rattaringluse_parklad_avaandmed/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson";
+    //https://dev.to/isalevine/three-ways-to-retrieve-json-from-the-web-using-node-js-3c88
+    https.get(link, (response) => {
+        let data = "";
+
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+
+        response.on("end", () => {
+            console.log(data);
+        });
+
+    }).on("error", (error) => {
+        console.error(error.message);
+    });
+}); */
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
